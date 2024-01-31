be506d9b8bfb1ea043307caa31a93b2e5099db71
Robin-infodeclat <robin.castellanos@infodeclat.com>
2024-01-31 13:55:23 -0500

    API CORE

---
+++ PR_ASP.API/Models/BDAPIContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PR_API_ASP.Models
+{
+    public partial class BDAPIContext : DbContext
+    {
+        public BDAPIContext()
+        {
+        }
+
+        public BDAPIContext(DbContextOptions<BDAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categoria> Categoria { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.IdCategoria)
+                    .HasName("PK__CATEGORI__4BD51FA504BCA169");
+
+                entity.ToTable("CATEGORIA");
+
+                entity.Property(e => e.IdCategoria).HasColumnName("ID_CATEGORIA");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("DESCRIPCION");
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.IdProducto)
+                    .HasName("PK__PRODUCTO__88BD03571A4BCA9B");
+
+                entity.ToTable("PRODUCTO");
+
+                entity.Property(e => e.IdProducto).HasColumnName("ID_PRODUCTO");
+
+                entity.Property(e => e.CodigoBarra)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("CODIGO_BARRA");
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("DESCRIPCION");
+
+                entity.Property(e => e.IdCategoria).HasColumnName("ID_CATEGORIA");
+
+                entity.Property(e => e.Marca)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("MARCA");
+
+                entity.Property(e => e.Precio)
+                    .HasColumnType("decimal(10, 2)")
+                    .HasColumnName("PRECIO");
+
+                entity.HasOne(d => d.oCategoria)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.IdCategoria)
+                    .HasConstraintName("FK_IDCATEGORIA");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
